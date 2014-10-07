8b186091264505ceb40db9faaaef803472cd3d59
hadley <h.wickham@gmail.com>
2014-10-07 06:11:48 -0500

    Basic encrypt-decrypt cycle working

--- R/encryption.R
+++ R/encryption.R
@@ -1,3 +1,42 @@
+encrypt <- function(.name, ..., .pkg = ".") {
+  pkg <- devtools::as.package(.pkg)
+  key <- my_key()
+
+  values <- list(...)
+  path <- file.path(pkg$path, "secure", paste0(.name, ".enc-rds"))
+
+#   if (file.exists(path)) {
+#     message("Merging with existing data")
+#     old_values <- decrypt(basename(path), pkg = pkg)
+#     values <- modifyList(old_values, values)
+#   }
+
+  ser <- serialize(values, connection = NULL)
+
+  enc <- PKI::PKI.encrypt(ser, key, "AES-256")
+#   dec <- PKI::PKI.decrypt(enc, key, "AES-256")
+#   all.equal(ser, dec)
+#   unserialize(dec)
+
+  writeBin(enc, path)
+}
+
+decrypt <- function(name, pkg = ".") {
+  pkg <- devtools::as.package(pkg)
+  key <- my_key()
+
+  if (!grepl("\\.enc-rds", name))
+  path <- file.path(pkg$path, "secure", paste0(name, ".enc-rds"))
+  if (!file.exists(path)) {
+    stop(path, " does not exist", call. = FALSE)
+  }
+
+  enc <- readBin(path, "raw", file.info(path)$size * 1.1)
+  dec <- PKI::PKI.decrypt(enc, key, "AES-256")
+
+  unserialize(dec)
+}
+
 recrypt <- function(pkg = ".", key = new_key()) {
   message("Re-encrypting all files with new key")
   pkg <- devtools::as.package(pkg)
@@ -22,6 +61,6 @@ my_key <- function(key = local_key(), pkg = ".") {
   me[[1]]$key
 }
 
-new_key <- function() {
-  charToRaw(paste(sample(c(letters, 1:10), 50, replace = TRUE), collapse = ""))
+new_key <- function(n = 50) {
+  as.raw(sample(255, n, rep = TRUE))
 }
