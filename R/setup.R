6a3d5d3ac997f1e6dcc8978c0b89146cffc299ee
hadley <h.wickham@gmail.com>
2014-10-07 07:46:59 -0500

    Rename use_securrr to use_secure. Add secure to suggests

--- R/setup.R
+++ R/setup.R
@@ -3,13 +3,14 @@
 #' This creates a \code{secure/} directory and adds it to \code{.Rbuildignore}
 #'
 #' @param pkg Path to package. Defaults to working directory.
-use_securrr <- function(pkg = ".") {
+use_secure <- function(pkg = ".") {
   pkg <- devtools::as.package(pkg)
 
   secure_path <- file.path(pkg$path, "secure")
   dir.create(secure_path, showWarnings = FALSE)
 
   devtools::use_build_ignore("secure", pkg = pkg)
+  devtools::use_package("secure", "Suggests", pkg = pkg)
 
   invisible(TRUE)
 }
