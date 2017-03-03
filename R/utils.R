17a0153dbd1b69cad98325d310eb438dc7456560
Andrie de Vries <apdevries@gmail.com>
2017-03-03 10:42:06 +0000

    Code formatting: insert line breaks to make code chunk more readable

--- R/utils.R
+++ R/utils.R
@@ -15,6 +15,11 @@ format.public.key <- function(x, ...) {
 # From Simon Urbanek
 fingerprint <- function(x) {
   der <- PKI::PKI.save.key(x, "DER")
-
-  PKI::PKI.digest(unlist(lapply(c(list(charToRaw("ssh-rsa")), PKI::ASN1.decode(PKI::ASN1.decode(der)[[2]])[2:1]), function(o) c(writeBin(length(o), raw(), endian="big"), o))), "MD5")
+  
+  PKI::PKI.digest(
+    unlist(
+      lapply(c(list(charToRaw("ssh-rsa")), PKI::ASN1.decode(PKI::ASN1.decode(der)[[2]])[2:1]), 
+             function(o) c(writeBin(length(o), raw(), endian="big"), o))
+    ), 
+    "MD5")
 }
