8ac8520408e4cb983febd1cda1ee90a528b5f947
Andrie de Vries <apdevries@gmail.com>
2017-03-03 12:18:42 +0000

    Rename path object to filepath #17

--- R/package.R
+++ R/package.R
@@ -1,8 +1,8 @@
 find_vault <- function(x = NULL) {
-  if (is.path(x)) {
+  if (is.filepath(x)) {
     return(x)
   }
-
+  
   if (is.null(x)) {
     # Assume you want the current directory
     if (file.exists("vault")) {
@@ -11,24 +11,33 @@ find_vault <- function(x = NULL) {
       path <- "inst/vault"
     } else {
       stop("Can't find vault/ or inst/vault in working directory",
-        call. = FALSE)
+           call. = FALSE)
     }
-
+    
   } else if (is.character(x)) {
     # Name of a package
-    path <- system.file("vault", package = x)
+    if (file.exists("vault")) {
+      path <- "vault"
+    } else {
+      path <- system.file("vault", package = x)
+    }
     if (identical(path, "")) {
       stop(x, " does not contain secure vault (inst/vault).", call. = FALSE)
     }
   } else {
     stop("Unknown input", call. = FALSE)
   }
-
+  
   check_dir(path)
-  structure(path, class = "path")
+  structure(path, class = c("filepath", "character"))
 }
 
-is.path <- function(x) inherits(x, "path")
+is.filepath <- function(x) inherits(x, "filepath")
+print.filepath <- function(x, ...) {
+  class(x) <- "character"
+  NextMethod(print, x, ...)
+}
+
 
 check_dir <- function(path) {
   if (!file.exists(path)) {
